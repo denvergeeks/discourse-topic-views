7e44197ea3037e473395c9855618d2626534eba0
denvergeeks <jen@denvergeeks.com>
2026-03-11 16:56:58 -0600

    Update topic-content-view.js

--- assets/javascripts/discourse/initializers/topic-content-view.js
+++ assets/javascripts/discourse/initializers/topic-content-view.js
@@ -5,11 +5,11 @@ export default {
 
   initialize() {
     withPluginApi("1.0.0", (api) => {
-      // On every page change, check if ?tc=1 is in the URL.
+      // On every page change, check if ?tcv=1 is in the URL.
       // If so, add 'tc-mode' to <body> so the SCSS hides all chrome.
       api.onPageChange((url) => {
         const hasTcParam =
-          new URLSearchParams(window.location.search).get("tc") === "1";
+          new URLSearchParams(window.location.search).get("tcv") === "1";
         document.body.classList.toggle("tc-mode", hasTcParam);
       });
     });
