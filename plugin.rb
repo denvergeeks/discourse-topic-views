c968f7d9dbcca50ef91b73b2bb2ee0bb69ac1da0
denvergeeks <jen@denvergeeks.com>
2026-03-18 01:49:00 -0600

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -1,13 +1,13 @@
-# name: discourse-topic-content-view
+# name: discourse-topic-views
 # about: Renders topic title + cooked content only, triggered via ?tcv=1 query param
 # version: 1.0.0
 # authors: @denvergeeks
-# url: https://github.com/denvergeeks/discourse-topic-content-view
+# url: https://github.com/denvergeeks/discourse-topic-views
 
-enabled_site_setting :topic_content_view_enabled
+enabled_site_setting :topic_views_enabled
 
 # SCSS adds body.tcv-mode which hides all chrome; JS initializer sets it when ?tcv=1 present
-register_asset "stylesheets/topic-content-view.scss", :desktop
+register_asset "stylesheets/topic-views.scss", :desktop
 
 # No custom Rails routes needed — we piggyback on existing /t/:slug/:id Discourse routing.
 # The ?tcv=1 query param is detected client-side by the initializer.
